02e6741b8751b103db87802a35ad5a214e59d8e6
AhmedSaif2 <ahmed.sa2424@gmail.com>
2022-09-01 03:43:12 +0200

    frontend: replacing absolute units with relative units and adding a container to CoursesSection

--- src/components/CoursesSection.js
+++ src/components/CoursesSection.js
@@ -5,19 +5,21 @@ import styles from "./coursesSectionStyles.module.css";
 
 function CoursesSection(props) {
   return (
-    <div className={styles.courses_section}>
-      <h2>Expand your career opportunities with Python</h2>
-      <p>
-        Take one of Udemy's range of Python courses and learn how to code using
-        this incredibly useful language. Its simple syntax and readability makes
-        Python perfect for Flask, Django, data science, and machine learning.
-        You'll learn how to build everything from games to sites to apps. Choose
-        from a range of courses that will appeal to both beginners and advanced
-        developers alike...
-      </p>
-      <button className={styles.explore}>Explore Python</button>
-      <CoursesList></CoursesList>
-    </div>
+    <section className="container">
+      <div className={styles.courses_section}>
+        <h2>Expand your career opportunities with Python</h2>
+        <p>
+          Take one of Udemy's range of Python courses and learn how to code
+          using this incredibly useful language. Its simple syntax and
+          readability makes Python perfect for Flask, Django, data science, and
+          machine learning. You'll learn how to build everything from games to
+          sites to apps. Choose from a range of courses that will appeal to both
+          beginners and advanced developers alike...
+        </p>
+        <button className={styles.explore}>Explore Python</button>
+        <CoursesList></CoursesList>
+      </div>
+    </section>
   );
 }
 
