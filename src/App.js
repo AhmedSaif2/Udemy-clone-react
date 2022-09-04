8c947941d12403387a93895e87882cf4122b4225
AhmedSaif2 <ahmed.sa2424@gmail.com>
2022-09-04 10:00:43 +0200

    frontend: adding offers section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CoursesSection from "./components/CoursesSection";
 import Navbar from "./components/Navbar";
+import OffersSection from "./components/OffersSection";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <OffersSection></OffersSection>
       <CoursesSection></CoursesSection>
     </div>
   );
